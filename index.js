736a3b13675744c0285ee49a846a6d7800040b17
ReginaChege <reginachegewairimu@gmail.com>
2023-07-06 06:55:01 +0300

    DOM-JS

---
+++ index.js
@@ -0,0 +1,18 @@
+let child=document.getElementById('container').children
+console.log({child})
+
+let lastChild=document.getElementById('container').lastChild
+console.log({lastChild})
+document.getElementById('hello').style.color='blue'
+document.getElementById('hello').style.textDecoration='underline'
+
+document.getElementById('text').innerHTML='Hello my name is Regina and i am 25 years old'
+
+let h2=document.createElement('h2')
+
+h2.innerHTML='I am new here'
+document.getElementById('container').appendChild(h2);
+let button=document.getElementById('button')
+button.addEventListener('click',function(){
+    button.innerHTML='Clicked '
+})
